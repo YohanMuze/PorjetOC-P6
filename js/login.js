23cdce335e773def676c32b29df81cdccd4eeb27
Yohan Muze <yoh.nbc@hotmail.fr>
2023-06-29 16:46:36 +0200

    add HTML/CSS login page

---
+++ js/login.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+/*
+let user = {
+    name: 'John',
+    surname: 'Smith'
+  };
+  
+  let response = await fetch('/article/fetch/post/user', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(user)
+  });
+  
+  let result = await response.json();
+  alert(result.message);
+*/
